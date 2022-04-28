c6b162973f1987809c81477b9f09d3446f450907
taran9999 <taran184@gmail.com>
2022-04-27 19:16:56 -0700

    Functions demo (complete)

--- chapter-1/functions.c
+++ chapter-1/functions.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int power(int, int); //declares function, prevents "implicit declaration of function." Called "function prototype."
+int power(int, int); //declares function, prevents "implicit declaration of function." Called "function prototype."git 
 
 main() {
     printf("%d\n", power(2, 5));
