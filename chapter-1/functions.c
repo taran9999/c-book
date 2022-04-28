471dd578afb9d4ec4cf4ad3b02e2758afba721ec
taran9999 <taran184@gmail.com>
2022-04-27 19:16:22 -0700

    Functions demo

---
+++ chapter-1/functions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int power(int, int); //declares function, prevents "implicit declaration of function." Called "function prototype."
+
+main() {
+    printf("%d\n", power(2, 5));
+}
+
+int power(int base, int n) { //equivalent to pow(x, y) function in standard library
+    int i, p;
+
+    p = 1;
+    for(i = 1; i <= n; i++) {
+        p *= base;
+    }
+    return p;
+}
+
+// return-type function-name(parameters if any)
